f945787ee779bc9904490aabf4090393b3216141
Samuel Francis Waweru Ndegwa <s.f.waweru@gmail.com>
2024-05-15 14:32:24 +0300

    Initial Commit

---
+++ question2.js
@@ -0,0 +1,20 @@
+/*
+
+Write a JavaScript function that takes a comma-separated numbers as input and returns the sum of those numbers. The function should validate the input:
+ 
+If any non-numeric characters are present (except commas), treat those values as zero.
+If the input string is empty, return zero.
+
+*/
+
+let numbers = " 1, 2, 4, 5, 6, 7, 8, 9, 10"
+
+function sum(input){
+    //split to array
+    console.log(input.split())
+    // see the array
+    // check for non numeric characters
+    //
+}
+
+sum(numbers)
